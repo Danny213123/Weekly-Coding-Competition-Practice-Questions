5a55b5229557ff8008341ce1c4341919ea17ba93
Danny213123 <73540329+Danny213123@users.noreply.github.com>
2020-11-30 15:09:06 -0500

    Create How many Sundays?.py

---
+++ How many Sundays?.py
@@ -0,0 +1,12 @@
+import datetime
+ 
+day, month, year = 1, 1, 1901
+ 
+sundays = 0
+ 
+for x in range (year, year + 100):
+  for y in range (1, 13):
+    ans = datetime.date(x, y, day)
+    if (ans.strftime("%A") == "Sunday"):
+      sundays += 1
+print(sundays)
