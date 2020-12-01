2254aebd2408f8097b4a19864d1d201b150056e8
Danny213123 <73540329+Danny213123@users.noreply.github.com>
2020-11-30 20:36:22 -0500

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -1,4 +1,4 @@
 import numpy as np
 totalFuel = 0
-for x in range(100): totalFuel += (np.floor(int(input()) / 3) - 2)
+for x in range(100): totalFuel += (np.ceil(int(input()) / 3) - 2)
 print(totalFuel)
